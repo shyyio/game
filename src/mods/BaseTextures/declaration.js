bc70661751cf12eba2cfe0abc6bb540fe5a2efee
Shy <me@shyy.io>
2026-07-18 21:29:48 -0400

    Split the sprite atlas into animated and main sheets

--- src/mods/BaseTextures/declaration.js
+++ src/mods/BaseTextures/declaration.js
@@ -1,12 +1,15 @@
 import {AbstractModDeclaration, TextureDefinition} from "@/sdk/common.js";
-import imageUrl from "./sprites.png";
-import jsonUrl from "./sprites.json";
+import animatedImageUrl from "./animated.png";
+import animatedJsonUrl from "./animated.json";
+import mainImageUrl from "./main.png";
+import mainJsonUrl from "./main.json";
 
 export class BaseTexturesDeclaration extends AbstractModDeclaration {
 
     get textureDefinitions() {
         return [
-            new TextureDefinition(imageUrl, jsonUrl)
+            new TextureDefinition(animatedImageUrl, animatedJsonUrl),
+            new TextureDefinition(mainImageUrl, mainJsonUrl)
         ];
     }
 }
